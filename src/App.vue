c299c8805303e8264a66530a2c58a02b54d51d79
Astonie Muk <amukywah@gmail.com>
2021-03-29 19:15:54 +0200

    added vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <!-- <v-app class="grey lighten-4">
+              <Navbar/>
+           </v-app>  -->
+          <Navbar/>
+          <router-view/>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+
+    </div>
+   
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components:{
+    Navbar, 
+  },
+    data(){
+      return {
+
+      }
+    }
+
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  
+   <div>
+      <v-toolbar>
+      <v-toolbar-side-icon  class="grey--text" > <v-btn depressed @click="drawer = !drawer">icon</v-btn> 
+      </v-toolbar-side-icon>
+      <v-toolbar-title class="text-uppercase grey--text" > 
+          <span class="font-weight-light">Todo</span>
+          <span>Ninja</span>
+         
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn depressed class="">Sign Out 
+        <!-- <v-icon>exit_to_app</v-icon> -->
+      </v-btn>
+
+    </v-toolbar>
+
+    <v-navigation-drawer app class="success" v-model="drawer" >
+          <p>Test</p>
+    </v-navigation-drawer>
+   </div>
+ 
+</template>
+
+<script>
+  export default {
+    name: 'Navbar',
+
+  data(){
+
+      return {
+        drawer:false
+      }
+    
+
+  }
+
+
+  }
+    
+</script>
