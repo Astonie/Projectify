446a55ac1c8aed43863c5d189f901f1b2e0b6e82
Astonie Muk <amukywah@gmail.com>
2021-03-30 09:20:55 +0200

    added a nav-drawer en dashbord

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,8 +6,8 @@
       <v-toolbar-title class="text-uppercase grey--text"  > 
           <span class="font-weight-light">Todo</span>
           <span>Ninja</span>
-         
       </v-toolbar-title>
+
       <v-spacer></v-spacer>
 
       <v-btn depressed class="">Sign Out 
@@ -28,15 +28,15 @@
       <v-divider></v-divider>
 
       <v-list dense nav>
-       <v-list-item-group dark >
-        <v-list-item v-for="link in links" :key="link.title"
+       <v-list-item-group color="red" >
+        <v-list-item v-for="link in links" :key="link.text"
                                       router :to="link.route">
           <!-- <v-list-item-icon>
             <v-icon>{{ link.icon }}</v-icon>
           </v-list-item-icon> --> 
       
           <v-list-item-title>
-           {{ link.title }}
+           {{ link.text }}
           </v-list-item-title>
       
         </v-list-item>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,35 @@
 <template>
   <div class="team">
     <h1 class="subheading grey--text my-5" >Dashboard</h1>
-    <v-container>
-      content
+    <v-container class="my-5" >
+        <v-card class="pa-3" flat v-for="project in projects" :key="project.title"  >
+          <v-layout row wrap :class="`pa-3 project ${project.status}`" >
+            <v-flex xs12 sm4 md6 >
+              <div :class="`caption  ${project.status}`" >Project title</div>
+              <div> {{project.title}} </div>
+            </v-flex>
+
+            <v-flex xs6 sm4 md2 >
+             <div class="caption" >Person</div>
+              <div> {{project.person}} </div>
+            </v-flex>
+
+            <v-flex xs6 sm4 md2 >
+            <div class="caption" >Due by</div>
+              <div> {{project.due}} 1</div>
+            </v-flex>
+
+            <v-flex xs2 sm4 md2 >
+            <div class="caption" >Status</div>
+              <div> {{project.status}} </div>
+            </v-flex>
+
+          </v-layout>
+          <v-divider></v-divider>
+        </v-card>
     </v-container>
   </div>
+
 </template>
 
 
@@ -13,6 +38,43 @@
 
 export default {
   name: 'Dashboard',
+
+  data(){
+    return{
+      projects:[
+        {title:'Work on the Frontend vuetify',person:'Aston',due:'31 March 2021',status:'Ongoing',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam tenetur impedit at, blanditiis veritatis incidunt commodi veniam esse dolorum, eius provident exercitationem ut laboriosam fuga ad soluta non accusamus molestiae.'
+        },
+            {title:'Reviw my Laravel API projects',person:'Aston',due:'31 March 2021',status:'Ongoing',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam tenetur impedit at, blanditiis veritatis incidunt commodi veniam esse dolorum, eius provident exercitationem ut laboriosam fuga ad soluta non accusamus molestiae.'
+        },
+            {title:'Work on Vuex',person:'Me',due:'9 April 2021',status:'Ongoing',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam tenetur impedit at, blanditiis veritatis incidunt commodi veniam esse dolorum, eius provident exercitationem ut laboriosam fuga ad soluta non accusamus molestiae.'
+        },
+            {title:'work on Content Management System',person:'Aston',due:'31 April 2021',status:'Ongoing',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam tenetur impedit at, blanditiis veritatis incidunt commodi veniam esse dolorum, eius provident exercitationem ut laboriosam fuga ad soluta non accusamus molestiae.'
+        }
+
+      ]
+    
+    }
+  }
  
 }
 </script>
+
+
+<style scoped >
+
+    .project.complete {
+      border-left: 4px solid #3cd12c
+    }
+    .project.ongoing {
+      border-left: 4px solid orange 
+    }
+    .project.overdue {
+      border-left: 4px solid tomato
+    }
+
+
+</style>
